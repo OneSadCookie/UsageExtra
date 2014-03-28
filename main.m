59e01ae481edaef05787794847d386a0894a8414
Alex Gray <alex@mrgray.com>
2014-03-28 14:41:10 -0400

    Moved CPU operations to separate class, modernized.
    Implemented KVO accessors for bindings / updates.. Migrated to ARC,
    ObjC 2.0, etc.

--- main.m
+++ main.m
@@ -1,12 +1,10 @@
+
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char const *argv[])
 {
-    id pool = [[NSAutoreleasePool alloc] init];
-    [[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
-        [NSNumber numberWithBool:YES], @"normalizeCPUUsage",
-        nil]];
-    [pool drain];
-
-    return NSApplicationMain(argc, argv);
+  @autoreleasepool {
+    [NSUserDefaults.standardUserDefaults registerDefaults:@{@"normalizeCPUUsage":@YES}];
+  }
+  return NSApplicationMain(argc, argv);
 }
